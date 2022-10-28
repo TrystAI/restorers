c9dbbcfcbf27c8234b7871e1027893ba6f69f8e8
soumik12345 <19soumik.rakshit96@gmail.com>
2022-10-28 19:37:22 +0530

    added residual context block

--- mirnetv2/model/rcb.py
+++ mirnetv2/model/rcb.py
@@ -4,7 +4,7 @@ from .utils import shape_list
 
 
 class ContextBlock(tf.keras.layers.Layer):
-    def __init__(self, channels, *args, **kwargs):
+    def __init__(self, channels: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.mask_conv = tf.keras.layers.Conv2D(1, kernel_size=1, padding="same")
@@ -26,7 +26,8 @@ class ContextBlock(tf.keras.layers.Layer):
         )
 
         context_mask = self.mask_conv(inputs)
-        context_mask = self.softmax(tf.reshape(context_mask, (-1, height * width, 1)))
+        context_mask = tf.reshape(context_mask, (-1, height * width, 1))
+        context_mask = self.softmax(context_mask)
         context_mask = tf.expand_dims(context_mask, axis=1)
 
         context = tf.reshape(
@@ -40,3 +41,27 @@ class ContextBlock(tf.keras.layers.Layer):
         channel_add_term = self.leaky_relu(channel_add_term)
         channel_add_term = self.channel_add_conv_2(channel_add_term)
         return inputs + channel_add_term
+
+
+class ResidualContextBlock(tf.keras.layers.Layer):
+    def __init__(self, channels: int, groups: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.conv_1 = tf.keras.layers.Conv2D(
+            channels, kernel_size=3, padding="same", groups=groups
+        )
+        self.conv_2 = tf.keras.layers.Conv2D(
+            channels, kernel_size=3, padding="same", groups=groups
+        )
+        self.leaky_relu = tf.keras.layers.LeakyReLU(alpha=0.2)
+
+        self.context_block = ContextBlock(channels=channels)
+
+    def call(self, inputs):
+        x = self.conv_1(inputs)
+        x = self.leaky_relu(x)
+        x = self.conv_2(x)
+        x = self.context_block(x)
+        x = self.leaky_relu(x)
+        x = x + inputs
+        return x
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 import tensorflow as tf
 
 from mirnetv2.model.skff import SelectiveKernelFeatureFusion
-from mirnetv2.model.rcb import ContextBlock
+from mirnetv2.model.rcb import ContextBlock, ResidualContextBlock
 
 
 class ModelTester(unittest.TestCase):
@@ -14,4 +14,11 @@ class ModelTester(unittest.TestCase):
     def test_context_block(self):
         x = tf.ones((1, 512, 512, 80))
         y = ContextBlock(channels=80)(x)
+        self.assertEqual(y.shape, (1, 512, 512, 80))
+    
+    def test_residual_context_block(self):
+        x = tf.ones((1, 512, 512, 80))
+        y = ResidualContextBlock(channels=80, groups=1)(x)
+        self.assertEqual(y.shape, (1, 512, 512, 80))
+        y = ResidualContextBlock(channels=80, groups=2)(x)
         self.assertEqual(y.shape, (1, 512, 512, 80))

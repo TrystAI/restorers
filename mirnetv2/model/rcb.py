97c6dd5b981175be943985256f0465efabdf78e1
soumik12345 <19soumik.rakshit96@gmail.com>
2022-10-28 15:08:18 +0530

    added context block

---
+++ mirnetv2/model/rcb.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+from .utils import shape_list
+
+
+class ContextBlock(tf.keras.layers.Layer):
+    def __init__(self, channels, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.mask_conv = tf.keras.layers.Conv2D(1, kernel_size=1, padding="same")
+
+        self.channel_add_conv_1 = tf.keras.layers.Conv2D(
+            channels, kernel_size=1, padding="same"
+        )
+        self.channel_add_conv_2 = tf.keras.layers.Conv2D(
+            channels, kernel_size=1, padding="same"
+        )
+
+        self.softmax = tf.keras.layers.Softmax(axis=1)
+        self.leaky_relu = tf.keras.layers.LeakyReLU(alpha=0.2)
+
+    def modeling(self, inputs):
+        _, height, width, channels = shape_list(inputs)
+        reshaped_inputs = tf.expand_dims(
+            tf.reshape(inputs, (-1, channels, height * width)), axis=1
+        )
+
+        context_mask = self.mask_conv(inputs)
+        context_mask = self.softmax(tf.reshape(context_mask, (-1, height * width, 1)))
+        context_mask = tf.expand_dims(context_mask, axis=1)
+
+        context = tf.reshape(
+            tf.matmul(reshaped_inputs, context_mask), (-1, 1, 1, channels)
+        )
+        return context
+
+    def call(self, inputs, *args, **kwargs):
+        context = self.modeling(inputs)
+        channel_add_term = self.channel_add_conv_1(context)
+        channel_add_term = self.leaky_relu(channel_add_term)
+        channel_add_term = self.channel_add_conv_2(channel_add_term)
+        return inputs + channel_add_term
--- tests.py
+++ tests.py
@@ -2,11 +2,16 @@ import unittest
 import tensorflow as tf
 
 from mirnetv2.model.skff import SelectiveKernelFeatureFusion
+from mirnetv2.model.rcb import ContextBlock
 
 
 class ModelTester(unittest.TestCase):
-    
     def test_skff(self):
         x = tf.ones((1, 256, 256, 120))
         y = SelectiveKernelFeatureFusion(channels=120)([x, x])
         self.assertEqual(y.shape, (1, 256, 256, 120))
+
+    def test_context_block(self):
+        x = tf.ones((1, 512, 512, 80))
+        y = ContextBlock(channels=80)(x)
+        self.assertEqual(y.shape, (1, 512, 512, 80))

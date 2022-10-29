c28caa5bbc832c12517a79d71e809863c01a7aaa
soumik12345 <19soumik.rakshit96@gmail.com>
2022-10-29 07:09:54 +0530

    added downsample block

---
+++ mirnetv2/model/downsample.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+
+
+class DownBlock(tf.keras.layers.Layer):
+    def __init__(self, channels: int, channel_factor: float, *args, **kwargs):
+        super(DownBlock, self).__init__(*args, **kwargs)
+        self.average_pool = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2)
+        self.conv = tf.keras.layers.Conv2D(
+            int(channels * channel_factor), kernel_size=1, strides=1, padding="same"
+        )
+
+    def call(self, inputs, *args, **kwargs):
+        return self.conv(self.average_pool(inputs))
+
+
+class DownSampleBlock(tf.keras.layers.Layer):
+    def __init__(
+        self, channels: int, scale_factor: int, channel_factor: int, *args, **kwargs
+    ):
+        super(DownSampleBlock, self).__init__(*args, **kwargs)
+        layers = []
+        for _ in range(np.log2(scale_factor)):
+            layers.append(DownBlock(channels, channel_factor))
+            channels = int(channels * channel_factor)
+        self.layers = tf.keras.Sequential(layers)
+
+    def call(self, inputs, *args, **kwargs):
+        return self.layers(inputs)
--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from mirnetv2.model.skff import SelectiveKernelFeatureFusion
 from mirnetv2.model.rcb import ContextBlock, ResidualContextBlock
+from mirnetv2.model.downsample import DownBlock, DownSampleBlock
 
 
 class ModelTester(unittest.TestCase):
@@ -15,10 +16,20 @@ class ModelTester(unittest.TestCase):
         x = tf.ones((1, 512, 512, 80))
         y = ContextBlock(channels=80)(x)
         self.assertEqual(y.shape, (1, 512, 512, 80))
-    
+
     def test_residual_context_block(self):
         x = tf.ones((1, 512, 512, 80))
         y = ResidualContextBlock(channels=80, groups=1)(x)
         self.assertEqual(y.shape, (1, 512, 512, 80))
         y = ResidualContextBlock(channels=80, groups=2)(x)
         self.assertEqual(y.shape, (1, 512, 512, 80))
+
+    def test_down_block(self):
+        x = tf.ones((1, 512, 512, 80))
+        y = DownBlock(channels=80, channel_factor=1.5)(x)
+        self.assertEqual(y.shape, (1, 256, 256, 120))
+    
+    def test_downsample_block(self):
+        x = tf.ones((1, 512, 512, 80))
+        y = DownBlock(channels=80, channel_factor=1.5)(x)
+        self.assertEqual(y.shape, (1, 256, 256, 120))

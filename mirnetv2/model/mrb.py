2d89b5d8c2a986c357e80f3215175e82c8c6ab79
soumik12345 <19soumik.rakshit96@gmail.com>
2022-10-29 08:57:25 +0530

    added multi-scale residual block

---
+++ mirnetv2/model/mrb.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+
+from .downsample import DownSampleBlock
+from .rcb import ResidualContextBlock
+from .skff import SelectiveKernelFeatureFusion
+from .upsample import UpSampleBlock
+
+
+class MultiScaleResidualBlock(tf.keras.layers.Layer):
+    def __init__(
+        self, channels: int, channel_factor: float, groups: int, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+
+        # Residual Context Blocks
+        self.rcb_top = ResidualContextBlock(
+            int(channels * channel_factor**0), groups=groups
+        )
+        self.rcb_middle = ResidualContextBlock(
+            int(channels * channel_factor**1), groups=groups
+        )
+        self.rcb_bottom = ResidualContextBlock(
+            int(channels * channel_factor**2), groups=groups
+        )
+
+        # Downsample Blocks
+        self.down_2 = DownSampleBlock(
+            channels=int((channel_factor**0) * channels),
+            scale_factor=2,
+            channel_factor=channel_factor,
+        )
+        self.down_4 = tf.keras.Sequential(
+            [
+                DownSampleBlock(
+                    channels=int((channel_factor**0) * channels),
+                    scale_factor=2,
+                    channel_factor=channel_factor,
+                ),
+                DownSampleBlock(
+                    channels=int((channel_factor**1) * channels),
+                    scale_factor=2,
+                    channel_factor=channel_factor,
+                ),
+            ]
+        )
+
+        # UpSample Blocks
+        self.up21_1 = UpSampleBlock(
+            channels=int((channel_factor**1) * channels),
+            scale_factor=2,
+            channel_factor=channel_factor,
+        )
+        self.up21_2 = UpSampleBlock(
+            channels=int((channel_factor**1) * channels),
+            scale_factor=2,
+            channel_factor=channel_factor,
+        )
+        self.up32_1 = UpSampleBlock(
+            channels=int((channel_factor**2) * channels),
+            scale_factor=2,
+            channel_factor=channel_factor,
+        )
+        self.up32_2 = UpSampleBlock(
+            channels=int((channel_factor**2) * channels),
+            scale_factor=2,
+            channel_factor=channel_factor,
+        )
+
+        # SKFF Blocks
+        self.skff_top = SelectiveKernelFeatureFusion(
+            channels=int(channels * channel_factor**0)
+        )
+        self.skff_middle = SelectiveKernelFeatureFusion(
+            channels=int(channels * channel_factor**1)
+        )
+
+        # Convolution
+        self.conv_out = tf.keras.layers.Conv2D(channels, kernel_size=1, padding="same")
+
+    def call(self, inputs, *args, **kwargs):
+        x_top = inputs
+        x_middle = self.down_2(x_top)
+        x_bottom = self.down_4(x_top)
+
+        x_top = self.rcb_top(x_top)
+        x_middle = self.rcb_middle(x_middle)
+        x_bottom = self.rcb_bottom(x_bottom)
+
+        x_middle = self.skff_middle([x_middle, self.up32_1(x_bottom)])
+        x_top = self.skff_top([x_top, self.up21_1(x_middle)])
+
+        x_top = self.rcb_top(x_top)
+        x_middle = self.rcb_middle(x_middle)
+        x_bottom = self.rcb_bottom(x_bottom)
+
+        x_middle = self.skff_middle([x_middle, self.up32_2(x_bottom)])
+        x_top = self.skff_top([x_top, self.up21_2(x_middle)])
+
+        output = self.conv_out(x_top)
+        output = output + inputs
+
+        return output
--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ from mirnetv2.model.downsample import DownBlock, DownSampleBlock
 from mirnetv2.model.rcb import ContextBlock, ResidualContextBlock
 from mirnetv2.model.skff import SelectiveKernelFeatureFusion
 from mirnetv2.model.upsample import UpBlock, UpSampleBlock
+from mirnetv2.model.mrb import MultiScaleResidualBlock
 
 
 class ModelTester(unittest.TestCase):
@@ -45,3 +46,8 @@ class ModelTester(unittest.TestCase):
         x = tf.ones((1, 128, 128, 180))
         y = UpSampleBlock(channels=180, scale_factor=2, channel_factor=1.5)(x)
         self.assertEqual(y.shape, (1, 256, 256, 120))
+
+    def test_mrb(self):
+        x = tf.ones((1, 512, 512, 80))
+        y = MultiScaleResidualBlock(channels=80, channel_factor=1.5, groups=1)(x)
+        self.assertEqual(y.shape, (1, 512, 512, 80))

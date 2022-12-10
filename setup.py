be0da344d561d9221d8a1c126dec7bdf49ee4e74
soumik12345 <19soumik.rakshit96@gmail.com>
2022-12-10 13:12:40 +0530

    made package installable + updated workflow

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+setuptools.setup(
+    name="restorers",
+    version="0.0.1",
+    description="Image Restoration toolkit in TensorFlow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/username/demopackage",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

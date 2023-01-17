96623aabac6d5c11e395c726848729a60fb9fbdb
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-01-17 16:12:42 +0530

    feat(tests/model): add type hints and drop redundant `shutil` import

--- restorers/tests/model/mirnetv2/test_mirnetv2.py
+++ restorers/tests/model/mirnetv2/test_mirnetv2.py
@@ -1,4 +1,3 @@
-import shutil
 import unittest
 
 import tensorflow as tf
@@ -13,49 +12,49 @@ from restorers.model.mirnetv2.upsample import UpBlock, UpSampleBlock
 
 
 class ModelTester(unittest.TestCase):
-    def test_skff(self):
+    def test_skff(self) -> None:
         x = tf.ones((1, 256, 256, 120))
         y = SelectiveKernelFeatureFusion(channels=120)([x, x])
         self.assertEqual(y.shape, (1, 256, 256, 120))
 
-    def test_context_block(self):
+    def test_context_block(self) -> None:
         x = tf.ones((1, 256, 256, 80))
         y = ContextBlock(channels=80)(x)
         self.assertEqual(y.shape, (1, 256, 256, 80))
 
-    def test_residual_context_block(self):
+    def test_residual_context_block(self) -> None:
         x = tf.ones((1, 256, 256, 80))
         y = ResidualContextBlock(channels=80, groups=1)(x)
         self.assertEqual(y.shape, (1, 256, 256, 80))
         y = ResidualContextBlock(channels=80, groups=2)(x)
         self.assertEqual(y.shape, (1, 256, 256, 80))
 
-    def test_down_block(self):
+    def test_down_block(self) -> None:
         x = tf.ones((1, 256, 256, 80))
         y = DownBlock(channels=80, channel_factor=1.5)(x)
         self.assertEqual(y.shape, (1, 128, 128, 120))
 
-    def test_downsample_block(self):
+    def test_downsample_block(self) -> None:
         x = tf.ones((1, 256, 256, 80))
         y = DownSampleBlock(channels=80, scale_factor=2, channel_factor=1.5)(x)
         self.assertEqual(y.shape, (1, 128, 128, 120))
 
-    def test_up_block(self):
+    def test_up_block(self) -> None:
         x = tf.ones((1, 128, 128, 180))
         y = UpBlock(channels=180, channel_factor=1.5)(x)
         self.assertEqual(y.shape, (1, 256, 256, 120))
 
-    def test_upsample_block(self):
+    def test_upsample_block(self) -> None:
         x = tf.ones((1, 128, 128, 180))
         y = UpSampleBlock(channels=180, scale_factor=2, channel_factor=1.5)(x)
         self.assertEqual(y.shape, (1, 256, 256, 120))
 
-    def test_mrb(self):
+    def test_mrb(self) -> None:
         x = tf.ones((1, 256, 256, 80))
         y = MultiScaleResidualBlock(channels=80, channel_factor=1.5, groups=1)(x)
         self.assertEqual(y.shape, (1, 256, 256, 80))
 
-    def test_rrg(self):
+    def test_rrg(self) -> None:
         x = tf.ones((1, 256, 256, 80))
         y = RecursiveResidualGroup(
             channels=80, num_mrb_blocks=2, channel_factor=1.5, groups=1
@@ -70,7 +69,7 @@ class ModelTester(unittest.TestCase):
         )(x)
         self.assertEqual(y.shape, (1, 256, 256, 80))
 
-    def test_mirnet_v2(self):
+    def test_mirnet_v2(self) -> None:
         x = tf.ones((1, 256, 256, 3))
         y = MirNetv2(
             channels=80,

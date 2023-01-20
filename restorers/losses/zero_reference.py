b0132bf39fb288c425dc1fe3c0d7a4dca9ed328b
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-01-20 20:22:48 +0000

    add: illumination smoothness loss + exposure control loss + corresponding unit tests

--- restorers/losses/zero_reference.py
+++ restorers/losses/zero_reference.py
@@ -12,3 +12,23 @@ def color_constancy(x):
         + tf.square(difference_red_blue)
         + tf.square(difference_green_blue)
     )
+
+
+def exposure_control_loss(x, window_size: int = 16, mean_val: float = 0.6):
+    x = tf.reduce_mean(x, axis=-1, keepdims=True)
+    mean = tf.nn.avg_pool2d(x, ksize=window_size, strides=window_size, padding="VALID")
+    return tf.reduce_mean(tf.square(mean - mean_val))
+
+
+def illumination_smoothness_loss(x):
+    batch_size = tf.shape(x)[0]
+    h_x = tf.shape(x)[1]
+    w_x = tf.shape(x)[2]
+    count_h = (tf.shape(x)[2] - 1) * tf.shape(x)[3]
+    count_w = tf.shape(x)[2] * (tf.shape(x)[3] - 1)
+    h_tv = tf.reduce_sum(tf.square((x[:, 1:, :, :] - x[:, : h_x - 1, :, :])))
+    w_tv = tf.reduce_sum(tf.square((x[:, :, 1:, :] - x[:, :, : w_x - 1, :])))
+    batch_size = tf.cast(batch_size, dtype=tf.float32)
+    count_h = tf.cast(count_h, dtype=tf.float32)
+    count_w = tf.cast(count_w, dtype=tf.float32)
+    return 2 * (h_tv / count_h + w_tv / count_w) / batch_size
--- restorers/tests/losses/zero_reference.py
+++ restorers/tests/losses/zero_reference.py
@@ -2,10 +2,22 @@ import unittest
 
 import tensorflow as tf
 
-from restorers.losses.zero_reference import color_constancy
+from restorers.losses.zero_reference import (
+    color_constancy,
+    exposure_control_loss,
+    illumination_smoothness_loss,
+)
 
 
 class ZeroReferenceLossesTest(unittest.TestCase):
     def test_color_constancy(self) -> None:
         x = tf.ones((1, 256, 256, 3))
         self.assertEqual(color_constancy(x).numpy().item(), 0.0)
+
+    def test_exposure_loss(self) -> None:
+        x = tf.ones((1, 256, 256, 3))
+        self.assertEqual(exposure_control_loss(x).numpy().item(), 0.36)
+
+    def test_illumination_smoothness_loss(self) -> None:
+        x = tf.ones((1, 256, 256, 3))
+        self.assertEqual(illumination_smoothness_loss(x).numpy().item(), 0.0)

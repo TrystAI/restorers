963a8d555e67bc29f091726472ad3384cd3ab047
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-01-26 20:27:03 +0000

    feat(utils): add type hints

--- restorers/utils.py
+++ restorers/utils.py
@@ -1,10 +1,13 @@
+from typing import List, Tuple
+
 import tensorflow as tf
 from absl import logging
 from matplotlib import pyplot as plt
+from PIL import Image
 from wandb.keras import WandbModelCheckpoint
 
 
-def initialize_device():
+def initialize_device() -> tf.distribute.Strategy:
     devices = tf.config.list_physical_devices("GPU")
     if len(devices) > 1:
         device_names = ", ".join([device.name for device in devices])
@@ -15,7 +18,9 @@ def initialize_device():
         return tf.distribute.OneDeviceStrategy(device="GPU:0")
 
 
-def get_model_checkpoint_callback(filepath, save_best_only: bool, using_wandb: bool):
+def get_model_checkpoint_callback(
+    filepath: str, save_best_only: bool, using_wandb: bool
+) -> tf.keras.callbacks.ModelCheckpoint:
     return (
         WandbModelCheckpoint(
             filepath=filepath,
@@ -35,14 +40,16 @@ def get_model_checkpoint_callback(filepath, save_best_only: bool, using_wandb: b
     )
 
 
-def scale_tensor(tensor):
+def scale_tensor(tensor: tf.Tensor) -> tf.Tensor:
     """Utility for scaling the values of a tensor in [0,1]"""
     _min = tf.math.reduce_min(tensor)
     _max = tf.math.reduce_max(tensor)
     return (tensor - _min) / (_max - _min)
 
 
-def plot_results(images, titles, figure_size=(12, 12)):
+def plot_results(
+    images: List[Image], titles: List[str], figure_size: Tuple[int, int] = (12, 12)
+) -> None:
     """A simple utility for plotting the results"""
     fig = plt.figure(figsize=figure_size)
     for i in range(len(images)):

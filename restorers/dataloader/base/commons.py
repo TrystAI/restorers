3d6d384616f8304172da9efcf5c5cfa88fcf5b50
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-02-07 17:48:12 +0000

    feedback: set normalization_factor to 1 by default in read_image

--- restorers/dataloader/base/commons.py
+++ restorers/dataloader/base/commons.py
@@ -23,7 +23,7 @@ def random_vertical_flip(low_image, enhanced_image):
     )
 
 
-def read_image(image_path: str, normalization_factor) -> tf.Tensor:
+def read_image(image_path: str, normalization_factor: float = 1.0) -> tf.Tensor:
     image = tf.io.read_file(image_path)
     image = tf.io.decode_image(image, channels=3, expand_animations=False)
     image = tf.cast(image, dtype=tf.float32) / normalization_factor

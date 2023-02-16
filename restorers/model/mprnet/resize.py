783b7519d96c86d09fadb95f13968d4311762f9e
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-02-16 21:29:57 +0000

    feat(model/mprnet): add resizing layers

---
+++ restorers/model/mprnet/resize.py
@@ -0,0 +1,129 @@
+from typing import Optional
+
+import tensorflow as tf
+
+
+class DownSample(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        channels: int,
+        channel_factor: float,
+        name: str = "DownSample Block",
+        *args,
+        **kwargs
+    ) -> None:
+        super().__init__(name=name, *args, **kwargs)
+
+        self.channels = channels
+        self.channel_factor = channel_factor
+
+        self.downsample = tf.keras.Sequential(
+            [
+                tf.keras.layers.UpSampling2D(size=0.5, interpolation="bilinear"),
+                tf.keras.layers.Conv2D(
+                    filters=int(self.channels * self.channel_factor),
+                    kernel_size=1,
+                    strides=1,
+                    padding="same",
+                    use_bias=False,
+                ),
+            ]
+        )
+
+    def call(self, inputs: tf.Tensor, training: Optional[bool] = None) -> tf.Tensor:
+        return self.downsample(inputs)
+
+    def get_config(self) -> dict:
+        config = super().get_config()
+        config.update(
+            {
+                "channels": self.channels,
+                "channel_factor": self.channel_factor,
+            }
+        )
+        return config
+
+
+class UpSample(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        channels: int,
+        channel_factor: float,
+        name: str = "UpSample Block",
+        *args,
+        **kwargs
+    ) -> None:
+        super().__init__(name=name, *args, **kwargs)
+
+        self.channels = channels
+        self.channel_factor = channel_factor
+
+        self.downsample = tf.keras.Sequential(
+            [
+                tf.keras.layers.UpSampling2D(size=2, interpolation="bilinear"),
+                tf.keras.layers.Conv2D(
+                    filters=int(self.channels // self.channel_factor),
+                    kernel_size=1,
+                    strides=1,
+                    padding="same",
+                    use_bias=False,
+                ),
+            ]
+        )
+
+    def call(self, inputs: tf.Tensor, training: Optional[bool] = None) -> tf.Tensor:
+        return self.downsample(inputs)
+
+    def get_config(self) -> dict:
+        config = super().get_config()
+        config.update(
+            {
+                "channels": self.channels,
+                "channel_factor": self.channel_factor,
+            }
+        )
+        return config
+
+
+class SkipUpSample(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        channels: int,
+        channel_factor: float,
+        name: str = "SkipUpSample Block",
+        *args,
+        **kwargs
+    ) -> None:
+        super().__init__(name=name, *args, **kwargs)
+
+        self.channels = channels
+        self.channel_factor = channel_factor
+
+        self.skipupsample = tf.keras.Sequential(
+            [
+                tf.keras.layers.UpSampling2D(size=2, interpolation="bilinear"),
+                tf.keras.layers.Conv2D(
+                    filters=int(self.channels // self.channel_factor),
+                    kernel_size=1,
+                    strides=1,
+                    padding="same",
+                    use_bias=False,
+                ),
+            ]
+        )
+
+    def call(
+        self, inputs: tf.Tensor, residual: tf.Tensor, training: Optional[bool] = None
+    ) -> tf.Tensor:
+        processed_input = self.skipupsample(inputs)
+        return tf.concat([processed_input, residual], axis=-1)
+
+    def get_config(self) -> dict:
+        config = super().get_config()
+        config.update(
+            {
+                "channels": self.channels,
+                "channel_factor": self.channel_factor,
+            }
+        )
+        return config

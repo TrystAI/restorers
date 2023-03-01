34d406ab4999137cb72c2e7685ea3a5e159c7d0b
soumik12345 <19soumik.rakshit96@gmail.com>
2023-03-01 12:42:00 +0000

    add: EnlightenGAN dataloader + respective unit tests

--- restorers/dataloader/__init__.py
+++ restorers/dataloader/__init__.py
@@ -1,2 +1,3 @@
 from .lol_dataloader import LOLDataLoader, UnsupervisedLoLDataloader
 from .mit_adobe_5k_dataloader import MITAdobe5KDataLoader
+from .enlightengan_loader import EnlightenGANDataLoader
---
+++ restorers/dataloader/enlightengan_loader.py
@@ -0,0 +1,37 @@
+import os
+from glob import glob
+from typing import Union
+
+from .base import UnsupervisedDatasetFactory
+from ..utils import fetch_wandb_artifact
+
+
+class EnlightenGANDataLoader(UnsupervisedDatasetFactory):
+    def __init__(
+        self,
+        image_size: int,
+        bit_depth: int,
+        val_split: float,
+        use_low_light_images_only: bool = True,
+        dataset_artifact_address: str = None,
+    ) -> None:
+        super().__init__(image_size, bit_depth, val_split)
+        self.use_low_light_images_only = use_low_light_images_only
+        self.dataset_artifact_address = dataset_artifact_address
+        self.fetch_dataset(self.val_split, self.dataset_artifact_address)
+
+    def fetch_dataset(self, val_split: float, dataset_artifact_address: str):
+        artifact_dir = fetch_wandb_artifact(
+            artifact_address=dataset_artifact_address, artifact_type="dataset"
+        )
+        train_a_paths = sorted(glob(os.path.join(artifact_dir, " trainA", "*")))
+        train_b_paths = sorted(glob(os.path.join(artifact_dir, " trainA", "*")))
+        input_image_paths = (
+            train_a_paths
+            if self.use_low_light_images_only
+            else train_a_paths + train_b_paths
+        )
+        self.num_data_points = len(input_image_paths)
+        num_train_images = int(self.num_data_points * (1 - val_split))
+        self.train_input_images = input_image_paths[:num_train_images]
+        self.val_input_images = input_image_paths[num_train_images:]

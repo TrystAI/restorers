a472f330641d14dff1fc2983d47ed3978ac245a1
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-03-11 23:24:27 +0000

    fix: unit tests

--- restorers/tests/losses/test_spatial_consistency_loss.py
+++ restorers/tests/losses/test_spatial_consistency_loss.py
@@ -9,6 +9,6 @@ class SpatialConsistencyLossTest(unittest.TestCase):
     def test_spatial_constancy_loss(self) -> None:
         x = tf.ones((1, 256, 256, 3))
         self.assertEqual(
-            tf.reduce_mean(SpatialConsistencyLoss()(x, x).numpy()).item(),
+            tf.reduce_mean(SpatialConsistencyLoss()(x, x)).numpy().item(),
             0.0,
         )

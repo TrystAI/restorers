ac107dfb8fbc3c66ebe3e3b7b74ab9868d765b8d
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-03-11 22:04:43 +0000

    fix: non-reference losses + spatial constancy loss

--- restorers/losses/spatial_consistency_loss.py
+++ restorers/losses/spatial_consistency_loss.py
@@ -1,33 +1,9 @@
 import tensorflow as tf
 
 
-class SpatialConsistencyLoss(tf.keras.losses.Loss):
-    """The Spatial Consistency Loss implemented as a `tf.keras.losses.Loss`.
-
-    The spatial consistency loss encourages spatial coherence of the enhanced image through
-    preserving the difference of neighboring regions between the input image and its enhanced
-    version. It is given by
-
-    $$L_{s p a}=\frac{1}{K} \sum_{i=1}^K \sum_{j \in \Omega(i)}\left(\left|\left(Y_i-Y_j\right)\right|-\left|\left(I_i-I_j\right)\right|\right)^2$$
-
-    where...
-
-    * K is the number of local regions
-    * $\Omega(i)$ is the four neighboring regions (top, down, left, right) centered at the region i
-    * Y and I are denoted as the average intensity value of the local region in the enhanced version and input image respectively
-
-    Reference:
-
-    1. [Zero-DCE: Zero-reference Deep Curve Estimation for Low-light Image Enhancement](https://openaccess.thecvf.com/content_CVPR_2020/papers/Guo_Zero-Reference_Deep_Curve_Estimation_for_Low-Light_Image_Enhancement_CVPR_2020_paper.pdf)
-    2. [Zero-Reference Learning for Low-Light Image Enhancement (Supplementary Material)](https://openaccess.thecvf.com/content_CVPR_2020/supplemental/Guo_Zero-Reference_Deep_Curve_CVPR_2020_supplemental.pdf)
-    3. [Official PyTorch implementation of Zero-DCE](https://github.com/Li-Chongyi/Zero-DCE/blob/master/Zero-DCE_code/Myloss.py#L29)
-    4. [Unofficial PyTorch implementation of Zero-DCE](https://github.com/bsun0802/Zero-DCE/blob/master/code/utils.py#L79-L109)
-    5. [Tensorflow implementation of Zero-DCE](https://github.com/tuvovan/Zero_DCE_TF/blob/master/src/loss.py#L38-L86)
-    6. [Keras tutorial for implementing Zero-DCE](https://keras.io/examples/vision/zero_dce/#spatial-consistency-loss)
-    """
-
-    def __init__(self, **kwargs) -> None:
-        super().__init__(reduction="none")
+class SpatialConsistencyLoss(losses.Loss):
+    def __init__(self, **kwargs):
+        super(SpatialConsistencyLoss, self).__init__(reduction="none")
 
         self.left_kernel = tf.constant(
             [[[[0, 0, 0]], [[-1, 1, 0]], [[0, 0, 0]]]], dtype=tf.float32
@@ -42,7 +18,8 @@ class SpatialConsistencyLoss(tf.keras.losses.Loss):
             [[[[0, 0, 0]], [[0, 1, 0]], [[0, -1, 0]]]], dtype=tf.float32
         )
 
-    def call(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+    def call(self, y_true, y_pred):
+
         original_mean = tf.reduce_mean(y_true, 3, keepdims=True)
         enhanced_mean = tf.reduce_mean(y_pred, 3, keepdims=True)
         original_pool = tf.nn.avg_pool2d(
@@ -82,5 +59,4 @@ class SpatialConsistencyLoss(tf.keras.losses.Loss):
         d_right = tf.square(d_original_right - d_enhanced_right)
         d_up = tf.square(d_original_up - d_enhanced_up)
         d_down = tf.square(d_original_down - d_enhanced_down)
-        spatial_constancy_loss = tf.reduce_mean(d_left + d_right + d_up + d_down)
-        return spatial_constancy_loss
+        return d_left + d_right + d_up + d_down

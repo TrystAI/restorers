40fe48f8cdb1e00a654f6046dc5fdecdb8c7347a
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-04-05 00:11:59 +0530

    fix: linting

--- restorers/losses/zero_reference.py
+++ restorers/losses/zero_reference.py
@@ -70,11 +70,7 @@ def illumination_smoothness_loss(x: tf.Tensor) -> tf.Tensor:
     """An implementation of the Illumination Smoothness Loss.
 
     The purpose of the illumination smoothness loss is to preserve the monotonicity relations between
-    neighboring pixels and it is applied to each curve parameter map. It is given by
-    
-    $$
-    L_{t v_{\mathcal{A}}}=\frac{1}{N} \sum_{n=1}^N \sum_{c \in \xi}\left(\left|\nabla_x \mathcal{A}_n^c\right|+\nabla_y \mathcal{A}_n^c \mid\right)^2, \xi=\{R, G, B\}
-    $$
+    neighboring pixels and it is applied to each curve parameter map.
 
     Reference:
 

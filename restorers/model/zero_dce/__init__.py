fbb03cc67502099685c53996882d0fd7f684594e
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-04-06 13:23:42 +0530

    fix: fast_zero_dce import

--- restorers/model/zero_dce/__init__.py
+++ restorers/model/zero_dce/__init__.py
@@ -1,3 +1,3 @@
 from .dce_layer import DeepCurveEstimationLayer, FastDeepCurveEstimationLayer
 from .zero_dce import ZeroDCE
-from fast_zero_dce import FastZeroDce
+from .fast_zero_dce import FastZeroDce
